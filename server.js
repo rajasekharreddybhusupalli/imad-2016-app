85354ebd7175cf3abdd072fa1aaca90fd08fb2f6
rajasekharreddybhusupalli <rajasekhar1997reddy@gmail.com>
2016-09-28 15:58:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+app.get('/ui/madi.png', fun
+
+
+ction (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
